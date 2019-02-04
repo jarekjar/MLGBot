8fc5f67d44c663b944fba2c4e763bcc0166e143c
jarekjar <jaredkjar@gmail.com>
2019-02-04 14:54:35 -0800

    pre changes

---
+++ MLGBot/Commands/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace MLGBot.Commands
+{
+    public class HelloWorld : ModuleBase<SocketCommandContext>
+    {
+        [Command("balls"), Alias("dick"), Summary("Hello world command")]
+        public async Task Hello()
+        {
+            await Context.Channel.SendMessageAsync("Eat my dick and balls, this is Jared's MLG discord app.");
+        }
+    }
+}
---
+++ MLGBot/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.Configuration;
+
+namespace MLGBot
+{
+    class Program
+    {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+
+        static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            //grab config
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("config.json");
+
+            var configuration = builder.Build();
+
+            //discord client
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+
+            });
+
+            //help commands
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), services: null);
+
+            Client.Ready += Client_Ready;
+
+            Client.Log += Client_Log;
+
+            string Token = configuration["SECRET"];
+
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("jared be hot.", "", ActivityType.Watching);
+        }
+
+        private async Task Client_Log(LogMessage msg)
+        {
+            Console.WriteLine($"{DateTime.Now} at {msg.Source} {msg.Message}");
+        }
+        
+        private async Task Client_MessageReceived(SocketMessage msg)
+        {
+            var Message = msg as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+            
+            if (Context.Message == null || Context.Message.Content == "") return;
+
+            if (Context.User.IsBot) return;
+
+            var pos = 0;
+
+            if (!(Message.HasCharPrefix('$', ref pos) || Message.HasMentionPrefix(Client.CurrentUser, ref pos))) return;
+
+            var Result = await Commands.ExecuteAsync(Context, pos, services: null);
+
+            if (!Result.IsSuccess)
+                Console.WriteLine($"{DateTime.Now} at Commands Something went wrong with executing a command. Text: {Context.Message.Content} | Error: {Result.ErrorReason}");
+        }
+
+    }
+}
